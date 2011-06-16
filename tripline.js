b2501eb90047e06123d9f632ec9c2c3f6ebbb997
Dhaivat Pandya <dhaivatpandya@gmail.com>
2011-06-16 16:12:12 +0000

    added document functions

---
+++ tripline.js
@@ -0,0 +1,23 @@
+var TL = function() {
+    this.d_gebi = document.getElementById;
+    
+    this.d_gebtn = document.getElementsByTagName;
+    
+    this.d_w = document.write;
+    
+    this.d_ce = document.createElement;
+    
+    this.d_ctn = document.createTextNode;
+    
+    this.d_ac = document.appendChild;
+    
+    this.d_o = document.open;
+    
+    this.d_ca = document.createAttribute;
+    
+    this.d_cdf = document.createDocumentFragment;
+    
+    this.d_efp = document.elementFromPoint;
+    
+    this.d_gebn = document.getElementsByName;
+};
