6fe2b2be3f1e0e9228c059c94d455f576b89e691
poincare <dhaivatpandya@gmail.com>
2011-07-27 06:51:14 +0000

    Simple commit

--- tripline.js
+++ tripline.js
@@ -1,76 +1,25 @@
-var TL = function() {
+TL = function() {
     //document functions
-    this.d_gebi = document.getElementById;
-    
-    this.d_gebtn = document.getElementsByTagName;
-    
-    this.d_w = document.write;
-    
-    this.d_ce = document.createElement;
-    
-    this.d_ctn = document.createTextNode;
-    
-    this.d_ac = document.appendChild;
-    
-    this.d_o = document.open;
-    
-    this.d_ca = document.createAttribute;
-    
-    this.d_cdf = document.createDocumentFragment;
-    
-    this.d_efp = document.elementFromPoint;
-    
-    this.d_gebn = document.getElementsByName;
-    
-    //window functions
-    this.a = alert;
-    
-    this.f = focus;
-    
-    this.o = open;
-    
-    this.pr = print;
-    
-    this.stin = setInterval;
-    
-    this.cnfm = confirm;
-    
-    this.fd = find;
-    
-    this.st = scrollTo;
-    
-    this.sb = scrollBy;
-    
-    this.stt = setTimeout;
-    
-    this.s = stop;
-    
-    //Array functions
-    this.addArrayFunctions = function() {
-        Array.prototype.cct = Array.prototype.concat;
-        
-        Array.prototype.jn = Array.prototype.join;
-        
-        Array.prototype.pp = Array.prototype.pop;
-        
-        Array.prototype.pu = Array.prototype.push;
-        
-        Array.prototype.rev = Array.prototype.reverse;
-        
-        Array.prototype.sh = Array.prototype.shift;
-        
-        Array.prototype.sl = Array.prototype.slice;
-        
-        Array.prototype.so = Array.prototype.sort;
-        
-        Array.prototype.spl = Array.prototype.splice;
-        
-        Array.prototype.toS = Array.prototype.toString;
-        
-        Array.prototype.unsh = Array.prototype.unshift;
-        
-        Array.prototype.valof = Array.prototype.valueOf;
+    //document.getElementById
+    this.dgebi = function (id) {
+        document.getElementById(id);
     };
+    
+    //document.getElementsByTagName
+    
+    //document.getElements
+    
+    //document.createAttribute
+    
+    //document.createComment
+    
+    //document.createElement
+    
+    //document.createTextNode
+    
+    //document.getElementsByClassName - No IE support
+    
+    
+    
+    
 };
-
-var _ = new TL();
