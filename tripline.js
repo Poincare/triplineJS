66ff2e187c6fb7e69031e7618d7cc8df80732af2
poincare <dhaivatpandya@gmail.com>
2011-07-27 07:02:59 +0000

    Basic document object functions

--- tripline.js
+++ tripline.js
@@ -2,24 +2,42 @@ TL = function() {
     //document functions
     //document.getElementById
     this.dgebi = function (id) {
-        document.getElementById(id);
+        return document.getElementById(id);
     };
     
     //document.getElementsByTagName
+    this.dgebtn = function(name) {
+        return document.getElementsByTagName(name);   
+    };
     
-    //document.getElements
+    //document.getElementsByName
+    this.dgebn = function(name) {
+        return document.getElementsByName(name);   
+    };
     
     //document.createAttribute
+    this.dca = function(attname) {
+        return document.createAttribute(attname);  
+    };
     
     //document.createComment
+    this.dcc = function(text) {
+        return document.createComment(text);   
+    };
     
     //document.createElement
+    this.dce = function(elname) {
+        return document.createElement(elname);   
+    };
     
     //document.createTextNode
+    this.dctn = function(text) {
+        return document.createTextNode(text);    
+    };
     
     //document.getElementsByClassName - No IE support
-    
-    
-    
+    this.dgebcn = function(clas) {
+        document.getElementsByClassName(clas);
+    };
     
 };
